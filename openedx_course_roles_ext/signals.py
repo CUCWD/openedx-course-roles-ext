20618c86c8cdb3d613c724739eadbdec214d6f47
Zachary Trabookis <ztraboo@clemson.edu>
2026-01-12 14:43:05 -0500

    feat: Ensure that we enroll Limited Staff, Staff, and Admins roles in the Discussion Admin role to all for `Gradebook > Filter > Cohort` dropdown interaction.
    It appears that the user managing the course would need to be added to the `Discussion Admin` course role whenever they need to filter `Gradebook > Filter > Cohort` dropdown to interact with. This change adds/removes this `Discussion Admin` role access when the other course management roles are added/removed.

--- openedx_course_roles_ext/signals.py
+++ openedx_course_roles_ext/signals.py
@@ -18,6 +18,9 @@ from common.djangoapps.student.roles import (
     CourseDataResearcherRole,
 )
 
+from openedx.core.djangoapps.django_comment_common.models import assign_role as comment_assign_role, Role as CommentRole
+from openedx.core.djangoapps.django_comment_common.utils import seed_permissions_roles as comment_seed_permissions_roles, are_permissions_roles_seeded as comment_are_permissions_roles_seeded
+
 TRIGGER_ROLES = {
     CourseInstructorRole.ROLE,   # 'instructor'
     CourseStaffRole.ROLE,        # 'staff'
@@ -98,4 +101,74 @@ def auto_remove_data_researcher(sender, instance, **kwargs):
         user=instance.user,
         course_id=instance.course_id,
         role=DATA_RESEARCHER_ROLE,
+    ).delete()
+
+
+@receiver(post_save, sender=CourseAccessRole)
+def auto_add_discussion_admin(sender, instance, created, **kwargs):
+    """
+    When a user is added as Limited Staff / Staff / Instructor for a course,
+    make sure they also get Discussion Admins for that same course.
+
+    This is needed to ensure that course team members have proper permissions
+    when viewing course discussions as well as
+    `Instructor Dashboard > Gradebook > Filter > Cohort` dropdown is selectable.
+    """
+    if not created:
+        return
+
+    if not _is_course_team_role(instance):
+        return
+
+    # Already has the Discussion Admin role for this course?
+    exists = CommentRole.user_has_role_for_course(
+        instance.user,
+        instance.course_id,
+        "Administrator",
+    )
+
+    if not exists:
+        logger.info(
+            f"Auto-adding Discussion Admin role for user {instance.user.id} "
+            f"in course {instance.course_id} due to assignment of role {instance.role}."
+        )
+
+        # Ensure discussion roles exist for this course (only needed once per course)
+        if not comment_are_permissions_roles_seeded(str(instance.course_id)):
+            comment_seed_permissions_roles(instance.course_id)
+
+        # Assign Discussion Admin
+        comment_assign_role(instance.course_id, instance.user, "Administrator")
+
+
+@receiver(post_delete, sender=CourseAccessRole)
+def auto_remove_discussion_admin(sender, instance, **kwargs):
+    """
+    When a Limited Staff / Staff / Instructor role is removed from a course, and the user
+    no longer has *any* of those roles for that course, remove Course Discussion Admin.
+    """
+    if not _is_course_team_role(instance):
+        return
+
+    # Does the user still have *any* course-level limited_staff/staff/instructor role?
+    still_course_team = CourseAccessRole.objects.filter(
+        user=instance.user,
+        course_id=instance.course_id,
+        role__in=TRIGGER_ROLES,
+    ).exists()
+
+    if still_course_team:
+        return
+
+    # Safe to remove Discussion Admin for this course
+    logger.info(
+        f"Auto-removing Discussion Admin role for user {instance.user.id} "
+        f"in course {instance.course_id} due to removal of role {instance.role}."
+    )
+
+    # Remove Discussion Admin
+    CommentRole.objects.filter(
+        users=instance.user,
+        course_id=instance.course_id,
+        name__in=["Administrator"],
     ).delete()
